7fc4119a9e94e6bb23402dd58a43a3f0e0180869
Jason Tralli <jtrallii@live.com>
2024-04-29 19:48:46 -0300

    Inicio de projeto

---
+++ src/main/java/jpa/spring/treinamento/pipoflix/demo/PipoFlixApplication.java
@@ -0,0 +1,13 @@
+package jpa.spring.treinamento.pipoflix.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipoFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipoFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa/spring/treinamento/pipoflix/demo/PipoFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.spring.treinamento.pipoflix.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipoFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
